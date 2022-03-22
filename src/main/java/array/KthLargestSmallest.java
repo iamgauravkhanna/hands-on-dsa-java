a8477b67739757745c117e8083bab774c0bf5ad4
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-03-22 20:44:28 +0530

    changes-9549

--- src/main/java/array/KthLargestSmallest.java
+++ src/main/java/array/KthLargestSmallest.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class KthLargestSmallest {
-    public static void main(String args[]){
-        int[] arr = {11,4,7,1,2,0,8,9};
-        findKthSmallest(arr,2);
-        findKthLargest(arr,1);
+    public static void main(String args[]) {
+        int[] arr = {11, 4, 7, 1, 2, 0, 8, 9};
+        findKthSmallest(arr, 2);
+        findKthLargest(arr, 1);
     }
 
-    private static void findKthSmallest(int[] arr,int position) {
+    private static void findKthSmallest(int[] arr, int position) {
         Arrays.sort(arr);
-        System.out.println("Kth Smallest : " + arr[position-1]);
+        System.out.println("Kth Smallest : " + arr[position - 1]);
     }
 
     private static void findKthLargest(int[] arr, int position) {
         Arrays.sort(arr);
-        System.out.println("Kth Largest : " + arr[arr.length-position]);
+        System.out.println("Kth Largest : " + arr[arr.length - position]);
     }
 }
