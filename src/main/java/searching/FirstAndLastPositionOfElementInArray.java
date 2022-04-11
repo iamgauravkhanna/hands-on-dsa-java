48b02e27d2f6159e5e6b72a2ba1236a7e4b85de1
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-04-11 09:09:54 +0530

    changes-1061

---
+++ src/main/java/searching/FirstAndLastPositionOfElementInArray.java
@@ -0,0 +1,23 @@
+package searching;
+
+public class FirstAndLastPositionOfElementInArray {
+
+    public static void main(String[] args) {
+        findPosition(new int[]{1, 3, 6, 7, 7, 7, 7, 11, 15}, 11);
+    }
+
+    private static void findPosition(int[] arr, int elementToFind) {
+        int start = -1, end = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (elementToFind != arr[i])
+                continue;
+            if (start == -1)
+                start = i;
+            end = i;
+        }
+        if (start != -1) {
+            System.out.println("First Position : " + start);
+            System.out.println("Last Position : " + end);
+        }
+    }
+}
