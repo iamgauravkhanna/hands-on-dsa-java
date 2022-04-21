3541162a7ab6ccb046d0e4f69579862b23958cc8
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-04-21 08:12:48 +0530

    changes-15042

--- src/main/java/sorting/InsertionSort.java
+++ src/main/java/sorting/InsertionSort.java
@@ -7,29 +7,40 @@ public class InsertionSort {
     public static void main(String[] args) {
 
         Random rand = new Random();
-        int[] numbers = new int[10];
+        int[] arr = new int[10];
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = rand.nextInt(1000000);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(1000000);
         }
 
         System.out.println("Before:");
-        printArray(numbers);
+        printArray(arr);
 
         int iteration = 0;
 
         //TODO
-
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+                iteration++;
+            }
+        }
 
         System.out.println("\nAfter:");
-        printArray(numbers);
-        System.out.println("Number of Iterations : " + iteration);
+        printArray(arr);
+        System.out.print("\nNumber of Iterations : " + iteration);
 
     }
 
     private static void printArray(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i]+" ");
+            System.out.print(numbers[i] + " ");
         }
     }
 }
