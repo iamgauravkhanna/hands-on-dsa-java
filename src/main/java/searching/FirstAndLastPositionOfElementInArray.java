0f27c3fd96083b27b0bff653aef5e06661c4ee59
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-06 08:39:48 +0530

    changes-19079

--- src/main/java/searching/FirstAndLastPositionOfElementInArray.java
+++ src/main/java/searching/FirstAndLastPositionOfElementInArray.java
@@ -3,11 +3,12 @@ package searching;
 public class FirstAndLastPositionOfElementInArray {
 
     public static void main(String[] args) {
-        findPosition(new int[]{1, 3, 6, 7, 7, 7, 7, 11, 15}, 11);
+        findPosition(new int[]{1, 3, 6, 7, 7, 7, 7, 1, 15}, 1);
     }
 
     private static void findPosition(int[] arr, int elementToFind) {
         int start = -1, end = -1;
+
         for (int i = 0; i < arr.length; i++) {
             if (elementToFind != arr[i])
                 continue;
@@ -15,6 +16,7 @@ public class FirstAndLastPositionOfElementInArray {
                 start = i;
             end = i;
         }
+
         if (start != -1) {
             System.out.println("First Position : " + start);
             System.out.println("Last Position : " + end);
--- src/main/java/searching/SearchElementInSortedRotatedArray.java
+++ src/main/java/searching/SearchElementInSortedRotatedArray.java
@@ -1,6 +1,6 @@
 package searching;
 
-public class SearchElementInRotatedArray {
+public class SearchElementInSortedRotatedArray {
 
     public static void main(String[] args) {
         int arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3, 4};
