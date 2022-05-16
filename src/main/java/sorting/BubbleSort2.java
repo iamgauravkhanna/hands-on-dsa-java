a2e13a71f8ce09dd98b9388931f7d0c5b95f0284
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-16 08:44:15 +0530

    changes-2022-05-16-08-44-15

--- src/main/java/sorting/BubbleSort2.java
+++ src/main/java/sorting/BubbleSort2.java
@@ -2,7 +2,7 @@ package sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class BubbleSort2 {
 
     public static void main(String args[]) {
 
@@ -14,14 +14,18 @@ public class BubbleSort {
         int iteration = 0;
 
         for (int i = 0; i < arr.length; i++) {
+            boolean isSwapped = false; // This flag is to break the loop if single swap hasn't happened
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
+                    isSwapped = true;
                 }
                 iteration++;
             }
+            if (!isSwapped)
+                break;
         }
 
         System.out.println("After:");
