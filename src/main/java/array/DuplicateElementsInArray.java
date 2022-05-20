d87f74bbdb643672a77a30e0238518dbadc4c33f
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-20 10:46:02 +0530

    changes-2022-05-20-10-46-02

--- src/main/java/array/DuplicateElementsInArray.java
+++ src/main/java/array/DuplicateElementsInArray.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 public class DuplicateElementsInArray {
 
     public static void main(String[] args) {
-        int[] arr = {6, 8, 1, 4, 4, 9, 6, 2, 9, 2, 0};
+
+        int[] arr = {6, 8, 1, 4, 4, 9, 6, 2, 9, 2, 0, 2};
+
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
         for (int element : arr) {
             if (hashMap.get(element) == null)
                 hashMap.put((Integer) element, 0);
@@ -15,6 +18,5 @@ public class DuplicateElementsInArray {
                 hashMap.put((Integer) element, hashMap.get(element) + 1);
             }
         }
-
     }
 }
