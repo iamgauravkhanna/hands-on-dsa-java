a046ab87015b4431f668a4f3d84ef7a6db3373d5
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-23 18:11:38 +0530

    changes-2022-05-23-18-11-38

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -11,8 +11,6 @@ public class BubbleSort {
         System.out.println("Before:");
         System.out.println(Arrays.toString(arr));
 
-        int iteration = 0;
-
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -20,12 +18,10 @@ public class BubbleSort {
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
-                iteration++;
             }
         }
 
         System.out.println("After:");
         System.out.println(Arrays.toString(arr));
-        System.out.println("\nNumber of Iterations : " + iteration);
     }
 }
