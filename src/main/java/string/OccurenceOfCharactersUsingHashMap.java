98646d6a51f1e79ae9b9288c8d004f730c6e7ccb
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-25 16:30:49 +0530

    changes-2022-05-25-16-30-48

--- src/main/java/string/OccurenceOfCharactersUsingHashMap.java
+++ src/main/java/string/OccurenceOfCharactersUsingHashMap.java
@@ -4,30 +4,19 @@ import java.util.HashMap;
 
 public class OccurenceOfCharactersUsingHashMap {
 
-    // 65 - 90 and 97 - 122
-
     private static void characterCount(String inputString) {
-        //Creating a HashMap containing char as a key and occurrences as a value
 
         HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
 
-        //Converting given string to char array
-
         char[] strArray = inputString.toCharArray();
-
-        //checking each char of strArray
 
         for (char c : strArray) {
             if (charCountMap.containsKey(c))
-                //If char 'c' is present in charCountMap, incrementing it's count by 1
                 charCountMap.put(c, charCountMap.get(c) + 1);
             else
-                //If char 'c' is not present in charCountMap,
-                //putting 'c' into charCountMap with 1 as it's value
                 charCountMap.put(c, 1);
         }
 
-        //Printing inputString and charCountMap
         System.out.println(inputString + " : " + charCountMap);
     }
 
@@ -38,5 +27,4 @@ public class OccurenceOfCharactersUsingHashMap {
 
         characterCount("Done And Gone");
     }
-
 }
