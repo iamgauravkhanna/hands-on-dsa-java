a20fd78de9351e39344262ee5e87041501ca4136
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-27 09:09:38 +0530

    changes-2022-05-27-09-09-38

---
+++ src/main/java/array/MaximumElementInASortedAndRotatedArray.java
@@ -0,0 +1,13 @@
+package array;
+
+public class MaximumElementInASortedAndRotatedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1, 2};
+        System.out.println("Maximum Element : " + findMax(arr, 0, arr.length - 1));
+    }
+
+    static int findMax(int[] arr, int low, int high) {
+        return 0;
+    }
+}
