c911c259720f0527b5d7649fe62f63fd8172fdb4
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-06-09 08:51:07 +0530

    changes-2022-06-09-08-51-07

---
+++ src/main/java/design_patterns/SingletonDesignPattern.java
@@ -0,0 +1,27 @@
+package design_patterns;
+
+public class SingletonDesignPattern {
+
+    private static SingletonDesignPattern singletonDesignPattern;
+
+    static {
+        singletonDesignPattern = new SingletonDesignPattern();
+    }
+
+    private SingletonDesignPattern() {
+		System.out.println("SingletonDesignPattern initalized due to static section");
+    }
+
+    public static SingletonDesignPattern getInstance() {
+        return singletonDesignPattern;
+    }
+
+    public void method() {
+        System.out.println("Hey.... it is working!!!");
+    }
+
+    public static void main(String a[]) {
+        SingletonDesignPattern instance = getInstance();
+        instance.method();
+    }
+}
