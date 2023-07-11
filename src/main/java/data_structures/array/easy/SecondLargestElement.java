65adb575f926531354e9691fd4897d0f2bf07cab
Gaurav Khanna <iamgauravkhanna@gmail.com>
2023-07-11 10:08:54 +0530

    changes-2023-07-11-10-08-54

---
+++ src/main/java/data_structures/array/easy/SecondLargestElement.java
@@ -0,0 +1,33 @@
+package data_structures.array.easy;
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+
+        // Expected 34
+        int[] arr1 = new int[]{12, 35, 1, 10, 34, 1};
+        findSecondLargestElement(arr1);
+
+        // Expected 5
+        int[] arr2 = new int[]{10, 5, 10};
+        findSecondLargestElement(arr2);
+
+        // Expected None
+        int[] arr3 = new int[]{10, 10, 10};
+        findSecondLargestElement(arr3);
+    }
+
+    private static void findSecondLargestElement(int[] arr) {
+        int first, second;
+        first = second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > first)
+                first = arr[i];
+            else if (arr[i] > second)
+                second = arr[i];
+        }
+        System.out.println("Largest Element :: " + first);
+        System.out.println("Second Largest Element :: " + second);
+    }
+}
--- src/main/java/data_structures/array/easy/ThreeLargestDistinctElements.java
+++ src/main/java/data_structures/array/easy/ThreeLargestDistinctElements.java
@@ -3,24 +3,25 @@ package data_structures.array.easy;
 public class ThreeLargestDistinctElements {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{6,5,56,45,23,5,9,7,4,10};
-        int first, second, third ;
+
+        int[] arr = new int[]{6, 5, 56, 45, 23, 5, 9, 7, 4, 10};
+
+        int first, second, third;
         first = second = third = Integer.MIN_VALUE;
 
-        for(int i = 0 ; i<arr.length;i++){
-            if(arr[i]>first){
-                third=second;
-                second=first;
-                first=arr[i];
-            }
-            else if (arr[i]>second){
-                third=second;
-                second=arr[i];
-            }
-            else if(arr[i]>third){
-                third=arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second) {
+                third = second;
+                second = arr[i];
+            } else if (arr[i] > third) {
+                third = arr[i];
             }
         }
+
         System.out.println("ThreeLargestDistinctElements :: " + first + ", " + second + ", " + third);
     }
 }
