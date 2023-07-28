ea45932ec68730155a6935778fb3f57b95a67557
Gaurav Khanna <iamgauravkhanna@gmail.com>
2023-07-28 17:35:49 +0530

    changes-2023-07-28-17-35-49

--- src/main/java/data_structures/array/easy/ThreeLargestDistinctElements.java
+++ src/main/java/data_structures/array/easy/ThreeLargestDistinctElements.java
@@ -5,9 +5,16 @@ public class ThreeLargestDistinctElements {
     public static void main(String[] args) {
 
         int[] arr = new int[]{6, 5, 56, 45, 23, 5, 9, 7, 4, 10};
+//        int[] arr = new int[]{6, 5};
 
         int first, second, third;
         first = second = third = Integer.MIN_VALUE;
+
+        /* There should be atleast three elements */
+        if(arr.length<3) {
+            System.out.println("Array Size not correct");
+            System.exit(0);
+        }
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > first) {
